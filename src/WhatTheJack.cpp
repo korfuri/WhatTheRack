3ca0209b39c54a938cafae5efbd9627ce29c1c4a
Uriel Corfa <korfuri@gmail.com>
2019-04-02 15:47:33 +0900

    Add WhatTheJack (WIP), a Wire randomizer.
    Single-button module: click and it adds a random wire between your
    existing modules. Fun idea but so far I find it lacking: the wires are
    just too random, and create a mess instead of an inspiring new
    constraint or idea.

---
+++ src/WhatTheJack.cpp
@@ -0,0 +1,86 @@
+#include "WhatTheRack.hpp"
+#include "plugin.hpp"
+#include "window.hpp"
+#include "CallbackButton.hpp"
+#include <random>
+
+struct WhatTheJack : Module {
+  enum ParamIds {
+    GENERATE_BUTTON,
+    NUM_PARAMS
+  };
+  enum InputIds {
+    NUM_INPUTS
+  };
+  enum OutputIds {
+    NUM_OUTPUTS
+  };
+  enum LightIds {
+    NUM_LIGHTS
+  };
+
+  WhatTheJack() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
+
+  void step() override {}
+
+  static void walkWidgetTree(Widget* root,
+			     std::vector<Port*>* inputs,
+			     std::vector<Port*>* outputs) {
+    for (Widget* w : root->children) {
+      Port* p = dynamic_cast<Port*>(w);
+      if (p) {
+	if (p->type == Port::INPUT) {
+	  if (!gRackWidget->wireContainer->getTopWire(p)) {
+	    inputs->push_back(p);
+	  }
+	} else {
+	  outputs->push_back(p);
+	}
+      } else {
+	walkWidgetTree(w, inputs, outputs);
+      }
+    }
+  }
+
+  void spawnWire() {
+    // List all input and output ports in the Rack
+    std::vector<Port*> inputs, outputs;
+    walkWidgetTree(gRackWidget, &inputs, &outputs);
+
+    if (outputs.size() < 1 || inputs.size() < 1) {
+      return;
+    }
+
+    // Pick a random input and output port
+    int iidx = std::rand() % inputs.size();
+    int oidx = std::rand() % outputs.size();
+
+    // Create a wire
+    WireWidget* w = new WireWidget();
+    w->inputPort = inputs[iidx];
+    gRackWidget->wireContainer->setActiveWire(w);
+    w->hoveredOutputPort = outputs[oidx];
+    gRackWidget->wireContainer->commitActiveWire();
+  }
+};
+
+typedef CallbackButton<WhatTheJack> CB;
+
+struct WhatTheJackWidget : ModuleWidget {
+  WhatTheJackWidget(WhatTheJack *module) : ModuleWidget(module) {
+    setPanel(SVG::load(assetPlugin(plugin, "res/WhatTheJack.svg"))); // fixme
+
+    addChild(Widget::create<ScrewSilver>(Vec(0, 0)));
+    addChild(Widget::create<ScrewSilver>(Vec(box.size.x - RACK_GRID_WIDTH, 0)));
+    addChild(Widget::create<ScrewSilver>(Vec(0, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+    addChild(Widget::create<ScrewSilver>(Vec(box.size.x - RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+    std::shared_ptr<rack::SVG> idle = SVG::load(assetPlugin(plugin, "res/BoomButton/question_color.svg"));
+    std::shared_ptr<rack::SVG> clicked = SVG::load(assetPlugin(plugin, "res/BoomButton/question_bw.svg"));
+    addChild(CB::create(Vec(50, 272), [](WhatTheJack* module){
+	  module->spawnWire();
+	}, module, idle, clicked));
+  }
+};
+
+Model *modelWhatTheJack = Model::create<WhatTheJack, WhatTheJackWidget>("WhatTheRack", "WhatTheJack", "WhatTheJack - Wire randomizer", UTILITY_TAG);
--- src/WhatTheRack.hpp
+++ src/WhatTheRack.hpp
@@ -9,3 +9,4 @@ extern Plugin *plugin;
 // Forward-declare each Model, defined in each module source file
 extern Model *modelWhatTheRack;
 extern Model *modelWhatTheMod;
+extern Model *modelWhatTheJack;
